8d318aec40b2fd7fab326a1374e2331bda5e0551
Kundentelefon <Kundentelefon@web.de>
2016-08-14 13:08:22 +0200

    Backup infinite Procedural Generation

---
+++ PerlinNoiseMap/Assets/Scripts/MapDisplay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapDisplay : MonoBehaviour
+{
+    public Renderer textRenderer;
+    public MeshFilter meshFilter;
+    public MeshRenderer MeshRenderer;
+
+    public void DrawTexture(Texture2D texture)
+    {
+        textRenderer.sharedMaterial.mainTexture = texture;
+        textRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
+    }
+
+    public void DrawMesh(MeshData meshData, Texture2D texture)
+    {
+        meshFilter.sharedMesh = meshData.CreateMesh();
+        MeshRenderer.sharedMaterial.mainTexture = texture;
+    }
+
+}
